859ec1b9d442a5475cd92d1fc108f517d45dc8e7
Akashi <akashi_sai@163.com>
2025-05-07 15:32:42 +0800

    :sparkles: support mdx

--- packages/memo-blogster-bobblegum/src/env.d.ts
+++ packages/memo-blogster-bobblegum/src/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="astro/client" />
+
+declare module 'fs-exists-cached' {
+  function exists(path: string): boolean;
+  function sync(path: string): boolean;
+  export default { exists, sync };
+}
