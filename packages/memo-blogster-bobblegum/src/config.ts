1864cae038f561afc034bf93297ac89b07c86bb0
Akashi <akashi_sai@163.com>
2025-05-07 17:25:40 +0800

    :wrench: chore: lint

--- packages/memo-blogster-bobblegum/src/config.ts
+++ packages/memo-blogster-bobblegum/src/config.ts
@@ -1,12 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
-export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+export const SITE_TITLE = 'Triple L'
+export const SITE_DESCRIPTION
+  = 'Welcome to my language learning log! I write about beginner language learners.'
+export const TWITTER_HANDLE = '@akashisai022'
+export const MY_NAME = 'Akashi'
 
 // setup in astro.config.mjs
-const BASE_URL = new URL(import.meta.env.SITE);
-export const SITE_URL = BASE_URL.origin;
+const BASE_URL = new URL(import.meta.env.SITE)
+export const SITE_URL = BASE_URL.origin
